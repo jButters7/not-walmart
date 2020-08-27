08004203435bbda1a1fb7f618326195a5eb71347
Jake Butters <jacobbutters@gmail.com>
2020-08-27 12:24:12 -0600

    backend complete

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -1,1 +1,87 @@
+const products = require('../data.json')
+
+const cart = { total: 0, items: [] };
+let cartId = 0;
+
+const updateCartTotal = () => {
+  const total = cart.items.reduce((acc, element) => {
+    return acc + (element.price * element.quantity)
+  }, 0)
+
+  cart.total = total.toFixed(2);
+}
+
+module.exports = {
+  getCart: (req, res) => {
+    res.status(200).send(cart)
+  },
+  addToCart: (req, res) => {
+    const { productId, quantity } = req.body
+
+    const index = cart.items.findIndex(element => element.id === +productId)
+
+    if (index === -1) {
+      const product = products.find(element => element.id === +productId)
+
+      product.cartId = cartId
+      product.quantity = +quantity
+
+      cart.items.push(product)
+
+      cartId++
+    } else {
+      cart.items[index].quantity += +quantity
+    }
+
+    updateCartTotal();
+
+    res.status(200).send(cart)
+  },
+
+  changeQuantity: (req, res) => {
+    const { cart_id } = req.params
+    const { action } = req.query
+
+    const index = cart.items.findIndex(element => element.cartId === +cart_id)
+
+    if (index === -1) {
+      return res.status(404).send('Product not in cart')
+    }
+
+    if (action === 'up') {
+      cart.items[index].quantity += 1;
+    } else {
+      if (cart.items[index].quantity > 1) {
+        cart.items[index].quantity -= 1
+      } else {
+        cart.items.splice(index, 1)
+      }
+    }
+
+    updateCartTotal()
+    res.status(200).send(cart)
+  },
+
+  removeFromCart: (req, res) => {
+    const { cart_id } = req.params
+
+    const index = cart.items.findIndex(element => element.cartId === +cart_id)
+
+    if (index === -1) {
+      return res.status(404).send('Item not in cart')
+    }
+    cart.items.splice(index, 1)
+
+    updateCartTotal();
+
+    res.status(200).send(cart);
+  },
+
+  checkout: (req, res) => {
+    cart.total = 0
+    cart.total = []
+    res.status(200).send(cart)
+  }
+}
+
 
--- server/index.js
+++ server/index.js
@@ -1,1 +1,22 @@
+const express = require('express')
+const app = express()
+const productCtrl = require('./controllers/productsController')
+const cartCtrl = require('./controllers/cartController')
 
+const SERVER_PORT = 5000;
+
+app.use(express.json())
+
+// Product Endpoints
+app.get('/api/products', productCtrl.getAllProducts)
+
+// Cart Endpoints
+app.get('/api/cart', cartCtrl.getCart)
+app.post('/api/cart', cartCtrl.addToCart)
+app.put('/api/cart/:cart_id', cartCtrl.changeQuantity)
+app.delete('/api/cart/:cart_id', cartCtrl.removeFromCart)
+app.delete('/api/cart', cartCtrl.checkout)
+
+
+
+app.listen(SERVER_PORT, () => console.log(`Mindless consumerism on port ${SERVER_PORT}`))
